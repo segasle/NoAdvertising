77280325fbe76d841eb7a1fa3fa454e7252a191d
sergej <segasle@gmail.com>
2019-03-17 20:53:22 +0300

    удаляет боковую

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-/**
- * Created by se on 17/03/2019.
- */
+$('#sideLeft').each(function () {
+    $(this).remove();
+});
+//.remove();
+//console.log(1);
+//adv-player not-ios vast flowplayer is-mouseout fp-default-playlist is-ready is-playing mini-banner-start adv-complete
+//main-player flowplayer fp-default-playlist not-ios not-mobile not-android subtitles-size-100 subtitles-color-black subtitles-text-color-white subtitles-opacity-75 is-ready is-paused is-mouseout
